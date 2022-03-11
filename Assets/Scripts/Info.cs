dac2e3671aefd1f1989c7a46c4827d1de0a31d58
Esther O Epelle <epelleoesther@gmail.com>
2022-03-11 15:36:48 +0100

    Hade Codex Example
    Maliyo Intern Assignment One

---
+++ Assets/Scripts/Info.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu (fileName ="New Info", menuName ="Infos")]
+public class Info : ScriptableObject
+{
+    public  string description;
+    public Sprite artwork;
+   
+}
---
+++ Assets/Scripts/InfoHistory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InfoHistory : MonoBehaviour
+{
+    public Text dtext;
+    public Image dImage;
+    // Start is called before the first frame update
+   public void Display(Info obj)
+    {
+        dtext.text = obj.description;
+        dImage.sprite = obj.artwork;
+    }
+
+}
